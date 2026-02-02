17b574d19e5866eaa79ffe2feba4317150d7305d
laflosh <l_florent_l@hotmail.fr>
2026-02-02 23:43:07 +0100

    Initialize spring application for project

---
+++ src/main/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bdd_manager.application_bdd_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBddManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBddManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdd_manager.application_bdd_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBddManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
