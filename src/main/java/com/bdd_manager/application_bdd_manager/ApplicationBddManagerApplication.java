43fd840a7e540562e31371e94d580ca8cbe7234c
laflosh <l_florent_l@hotmail.fr>
2026-02-03 00:36:26 +0100

    add depedencies and configuration project with different .properties profil

--- src/main/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplication.java
+++ src/main/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplication.java
@@ -1,13 +1,21 @@
 package com.bdd_manager.application_bdd_manager;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationBddManagerApplication {
 
+	private static final Logger log = LogManager.getLogger("ApplicationBddManagerApplication");
+	
 	public static void main(String[] args) {
+		
+		log.info("--- Initialize BDD Manager Application ---");
+		
 		SpringApplication.run(ApplicationBddManagerApplication.class, args);
+		
 	}
 
 }
--- src/test/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplicationTests.java
+++ src/test/java/com/bdd_manager/application_bdd_manager/ApplicationBddManagerApplicationTests.java
@@ -2,8 +2,10 @@ package com.bdd_manager.application_bdd_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ApplicationBddManagerApplicationTests {
 
 	@Test
