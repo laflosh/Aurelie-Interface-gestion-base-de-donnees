dfc89cb34fa893835c536f93763495b1d0cc92be
laflosh <l_florent_l@hotmail.fr>
2026-03-17 22:49:00 +0100

    add method to save a new report in database

---
+++ src/main/java/com/bdd_manager/application_bdd_manager/controller/ReportController.java
@@ -0,0 +1,53 @@
+package com.bdd_manager.application_bdd_manager.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bdd_manager.application_bdd_manager.model.Report;
+import com.bdd_manager.application_bdd_manager.model.dto.ReportDto;
+import com.bdd_manager.application_bdd_manager.service.ReportService;
+
+/**
+ * 
+ */
+@RestController
+@RequestMapping("/api")
+public class ReportController {
+
+	private final Logger log = LogManager.getLogger(ReportController.class);
+	
+	@Autowired
+	ReportService reportService;
+	
+	/**
+	 * @param report
+	 * @return
+	 */
+	@PostMapping("/report")
+	public ResponseEntity<Report> addReportInDatabase(ReportDto report){
+		
+		try {
+			
+			log.info("Trying to add a new report in database");
+			
+			Report addReport = reportService.addReportInDatabase(report);
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(addReport);
+			
+		} catch(Exception e){
+			
+			e.printStackTrace();
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+			
+		}
+		
+	}
+	
+}
---
+++ src/main/java/com/bdd_manager/application_bdd_manager/service/ReportService.java
@@ -0,0 +1,65 @@
+package com.bdd_manager.application_bdd_manager.service;
+
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bdd_manager.application_bdd_manager.model.Report;
+import com.bdd_manager.application_bdd_manager.model.dto.ReportDto;
+import com.bdd_manager.application_bdd_manager.repository.ReportRepository;
+
+/**
+ * 
+ */
+@Service
+public class ReportService {
+
+	private final Logger log = LogManager.getLogger(ReportService.class);
+	
+	@Autowired
+	ReportRepository reportRepository;
+
+	/**
+	 * @param report
+	 * @return
+	 */
+	public Report addReportInDatabase(ReportDto dto) {
+		
+		Date date = new Date();
+		dto.setDate(date);
+		
+		Report report = dtoToObjectReport(dto);
+		
+		log.info("Add new report in database");
+		
+		Report addReport = reportRepository.save(report);
+		
+		return addReport;
+		
+	}
+	
+	/**
+	 * @param dto
+	 * @return
+	 */
+	private Report dtoToObjectReport(ReportDto dto) {
+		
+		Report report = new Report();
+		
+		if(dto.getId() != null) {
+			report.setId(dto.getId());
+		}
+		report.setSubject(dto.getSubject());
+		report.setMessage(dto.getMessage());
+		report.setDate(dto.getDate());
+		report.setEmergency(dto.getEmergency());
+		report.setPicture(dto.getPicture());
+		
+		return report;
+		
+	}
+	
+}
